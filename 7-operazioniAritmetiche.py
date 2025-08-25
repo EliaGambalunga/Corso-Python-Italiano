6bba2a8ede4aa421fcc3b0d38d23233c44ed3ed5
EliaGambalunga <Eliagambalunga9@gmail.com>
2025-08-25 12:06:11 +0200

    Create 7-operazioniAritmetiche.py

---
+++ 7-operazioniAritmetiche.py
@@ -0,0 +1,27 @@
+
+x = 5
+y = 10
+#come operazioni aritmetiche possiamo usare:
+# + somma  5 + 10 = 15
+# - sottrazione 10 - 5 = 5
+# * moltiplicazione 5 * 10 = 50
+# / divisione 10 / 5 = 2.0 (il risultato è sempre un float)
+# // flore division 10 // 3 = 3 (il risultato è sempre un int, arrotondato per difetto)
+# ** potenza 5 ** 2 = 25
+# % il modulo 10 % 3 = 1 (il resto della divisione)
+#operatori di assegnamento
+x += 5 #equivale a x = x + 5
+print(x) #manda a schermo 10
+x -= 2 #equivale a x = x - 2
+print(x) #manda a schermo 8
+x *= 2 #equivale a x = x * 2
+#e così via per gli altri operatori
+#metodi min e max
+print(min(5,10,2,3,1)) #manda a schermo 1 cioè il valore minimo
+print(max(5,10,2,3,1)) #manda a schermo 10 cioè il valore massimo
+#metodo abs che restituisce il valore assoluto
+print(abs(-5)) #manda a schermo 5
+print(abs(5)) #manda a schermo 5
+#metodo pow che restituisce la potenza
+print(pow(5,2)) #manda a schermo 25 cioè 5 elevato alla 2
+print(pow(2,3)) #manda a schermo 8 cio
