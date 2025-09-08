534276a024f05bfb8d548ddebdcfc4a68d9d553f
EliaGambalunga <Eliagambalunga9@gmail.com>
2025-09-08 11:47:02 +0200

    Aggiunta capitolo 5

---
+++ collezioniDiDati.py
@@ -0,0 +1,20 @@
+#lezione teorica
+# ci sono 4 tipi di collezioni di dati in python
+# liste, tuple, set, dizionari
+#ognuna si indica con parentesi diverse perchè differiscono delle proprietà
+# liste -> []
+# tuple -> ()
+# set -> {}
+# dizionari -> {chiave:valore}
+
+#spiegazione temini:
+#ordinata -> gli elementi hanno un ordine preciso, l'aggiunta di elementi non incide sull'ordine
+città = ["milano", "roma", "napoli"] #esempio, se aggiungo "torino" alla fine della lista, rimane sempre l'ultimo elemento e milano il primo,
+#ma se lo aggiungo all'inizio, milano diventa il secondo e torino il primo
+# indicizzata -> gli elementi hanno un indice, cioè una posizione numerica che parte da 0, quindi milano è l'elemento 0, roma l'elemento 1...
+# modificabile -> posso aggiungere, togliere o modificare gli elementi creati una volta creata la collezione
+
+#liste sono collezioni ordinate, modificabili. Permettono duplicati
+#dizionari sono collezioni ordinate, modificabili. Non permettono duplicati
+#set sono collezioni non oridinate, non indicizzate. Non permettono duplicati
+#tuple sono collezioni ordinate ma immutabili. Permettono duplicati
